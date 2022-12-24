f5cb9fb86bc99c7c64f769712d9034fbb7d722d1
Vladislav Pavlov <vladislawpavlov@gmail.com>
2022-12-24 10:53:56 -0500

    feat: изменен порт с 3001 на 3000

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ const corsOptions = {
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use('*', cors(corsOptions));
@@ -81,5 +81,3 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT);
-
-// тест
